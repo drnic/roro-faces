f232bcbc179a4bc7c5eaa005f21cd2067507d3f5
Dr Nic Williams <drnicwilliams@gmail.com>
2009-04-01 08:24:36 +1000

    updated roro to rails 2.3.2

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,15 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '1.2.2' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '1.2.2' unless defined? RAILS_GEM_VERSION (LEAVE FOR HISTORY SAKE)
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 # Dependencies.log_activity = true
 
-require 'shared_secret'
+# require 'shared_secret'
 require 'active_support'
 
 module FacesConfig
